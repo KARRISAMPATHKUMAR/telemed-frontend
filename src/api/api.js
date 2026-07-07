1f967ffe1b433dc3d619a4757088a38a5fc00aac
KARRI SAMPATH KUMAR <2400030749@kluniversity.in>
2026-07-07 22:48:33 +0530

    connect

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+  baseURL: "https://telemed-backend-w06s.onrender.com",
 });
 
 export default api;
